c1cee1e279d8bbfb04b7d6a7f0476ef6fe31cf0e
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-11-15 20:35:44 -0600

    add props.title & props.children

--- src/Elements/Card.js
+++ src/Elements/Card.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import styled, { css } from 'styled-components';
 import { elevation, transition, colors, black } from 'Utilities';
 
-export const Card = styled.div`
+export const Card = props => {
+  return (
+    <Paper style={props.style}>
+      {props.title && (
+        <div>
+          <h3>{props.title}</h3>
+          <hr />
+        </div>
+      )}
+      <span>{props.children}</span>
+    </Paper>
+  );
+};
+
+const Paper = styled.div`
   background: white;
   border-radius: 5px;
   // color: ${colors.teal};
-  color: ${black};
+	color: ${black};
+	height: fit-content;
+	width: fit-content;
   padding: 15px;
   ${elevation[4]};
   ${transition({})};
